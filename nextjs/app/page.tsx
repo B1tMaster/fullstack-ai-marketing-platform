6dd99114f0ead978b7ff69182b9d2268bae169db
Dmitry <dima.avramenko@gmail.com>
2024-10-28 12:54:43 +0800

    Initial commit

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+//import Image from "next/image";
+export default function Home() {
+  return (
+    <div>
+      <div className="bg-red-500 text-white">Hello from new project</div>
+      <Button variant="default">Click me</Button>
+    </div>
+  );
+}
