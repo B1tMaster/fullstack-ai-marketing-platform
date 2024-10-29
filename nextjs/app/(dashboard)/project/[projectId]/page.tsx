0deab860598dd3bde266915701b8797eb529c1d0
Dmitry <dima.avramenko@gmail.com>
2024-10-29 12:40:19 +0800

    added dashboard and template detail

---
+++ nextjs/app/(dashboard)/project/[projectId]/page.tsx
@@ -0,0 +1,18 @@
+import { notFound } from "next/navigation";
+import React from "react";
+
+interface ProjectPageProps {
+  params: {
+    projectId: string;
+  };
+}
+
+export default async function ProjectPage({ params }: ProjectPageProps) {
+  const { projectId } = await params;
+
+  if (projectId != "123") {
+    return notFound();
+  }
+
+  return <div>ProjectPage: {projectId} </div>;
+}
