fb0d962cacb7efb664854b5021062c31a7c6b868
Dmitry <dima.avramenko@gmail.com>
2024-11-25 15:13:22 +0800

    implemented postgress integration, loading page, creation of projects

---
+++ nextjs/server/db/index.ts
@@ -0,0 +1,9 @@
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { config } from "dotenv";
+import { sql } from "@vercel/postgres";
+
+import * as schema from "./schema";
+
+config({ path: ".env" }); // or .env
+
+export const db = drizzle(sql, { schema });
