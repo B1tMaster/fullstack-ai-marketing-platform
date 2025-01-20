bb6b025b4a28a2c2503d0c5c0e32f8eea8745eb4
Dmitry <dima.avramenko@gmail.com>
2025-01-20 20:14:22 +0800

    feat: Add tokenHelper utility for token counting in Next.js

---
+++ nextjs/utils/tokenHelper.ts
@@ -0,0 +1,8 @@
+//Used to help with token counting
+// use js-tiktoken to count tokens
+ Documentation:  https://github.com/dqbd/tiktoken#nextjs
+
+ // create function: export const getPromptTokenCount(prompt: string): number 
+
+
+
