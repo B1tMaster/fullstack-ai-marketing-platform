a44cfd28a58cd4c4ddac6be687d81d3c61cb432a
Dmitry <dima.avramenko@gmail.com>
2025-01-20 18:03:26 +0800

    refactor: Remove unused MAX_TOKENS_PROMPTS constant

--- nextjs/lib/constants.ts
+++ nextjs/lib/constants.ts
@@ -1,4 +1,3 @@
 export const TERMINAL_STATUS = ["completed", "max_attempts_exceeded"];
 export const MAX_TOKENS_ASSETS = 100000;
 export const MAX_TOKENS_PROMPT = 20000;
-export const MAX_TOKENS_PROMPTS = 50000; // New constant for prompts
